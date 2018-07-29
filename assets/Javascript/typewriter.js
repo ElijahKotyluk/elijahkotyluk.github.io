d8ac5c7de437b1ad5aa65c429871c081493a33f1
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-07-29 11:33:56 -0700

    updated portfolio

---
+++ assets/Javascript/typewriter.js
@@ -0,0 +1,5 @@
+new TypeIt('#typewriter', {
+    strings: ['Hello world...', 'My name is Elijah Kotyluk, and I\'m from Sacramento, California. I am currently enrolled in the U.C. Berkeley coding program to become a full-stack developer and polish up some skills. My set of skills range from: HTML, CSS, Javascript, and Python. More specifically my time has been invested in Vue.js, Flask, App Engine, SCSS, and Webpack.'],
+    speed: 120,
+    breakLines: true
+})
